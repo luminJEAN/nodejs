8cba0474cda87a0b5e0d0355e531678095a7486f
jean <97730267+luminJEAN@users.noreply.github.com>
2022-01-17 10:38:58 +0800

    优化方法

--- express_project/router_handler/articleList_handler.js
+++ express_project/router_handler/articleList_handler.js
@@ -56,4 +56,52 @@ module.exports.getDetail = (req, res) => {
 }
 
 
+module.exports.delete = (req, res) => {
+    const id = req.params.id
+    const sqlDelete = 'update ev_articles set is_delete = 1 where id = ?'
+    db.query(sqlDelete, id, (err, result) => {
+        if (err) return res.cc(err)
+        if (result.affectedRows !== 1) {
+            return res.cc('删除失败', 500)
+        }
+        res.cc('删除成功', 200)
+    })
+
+}
+
+module.exports.update = (req, res) => {
+    const id = req.params.id
+    const data = {
+        title: req.body.title,
+        content: req.body.content,
+        state: req.body.state,
+        pub_data: req.body.time,
+        cover_img: req.body.img,
+        cate_id: req.body.cateId,
+        author_id: req.body.authorId
+    }
+    //数据库是否存在一样的title
+    const sqlSelect = 'select * from ev_articles where id <> ? and title = ?'
+    db.query(sqlSelect, [id, data.title], (err, result) => {
+        if (err) {
+            return res.cc(err)
+        }
+        if (result.length === 1) {
+            return res.cc('title 已存在，请重新输入！')
+        }
+        const sqlInsert = 'update ev_articles set ? where id = ?'
+        db.query(sqlInsert, [data, id], (err, result) => {
+            if (err) {
+                return res.cc(err)
+            }
+            if (result.affectedRows !== 1) {
+                return res.cc('更新失败')
+            }
+            res.cc('更新成功', 200)
+        })
+    })
+
+}
+
+
 
